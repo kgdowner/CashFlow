78074149174ce83fdcedc1888b8edbb089601fd5
John Miller <john.miller415@myci.csuci.edu>
2018-03-28 12:48:05 -0700

    Added category class to be used in transaction

---
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Category.java
@@ -0,0 +1,45 @@
+package edu.csuci.myci.cashflow;
+
+/**
+ * Created by john.miller415 on 3/28/18.
+ */
+
+public class Category {
+
+    private String categoryName;
+    private int categoryId;
+
+    public Category(String categoryName, int categoryId){
+        this.categoryName = categoryName;
+        this.categoryId= categoryId;
+    }
+
+    public String getCategoryName(){
+        return this.categoryName;
+    }
+
+    public int getCategoryId(){
+        return this.categoryId;
+    }
+
+    public void setCategoryName(String categoryName){
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryId(int categoryId){
+        this.categoryId = categoryId;
+    }
+
+    public String toString(){
+        return ("Category: " + categoryName + "\tID: " + categoryId);
+    }
+
+    public boolean equals(Category inputCategory) {
+        if(this.categoryName.equalsIgnoreCase(inputCategory.getCategoryName())
+                && this.categoryId == inputCategory.getCategoryId() ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
