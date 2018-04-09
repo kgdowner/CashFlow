2bd9710bb27602c6f89fe05fac63ec3cbf5981d2
kgdowner <keith.downer897@myci.csuci.edu>
2018-04-09 13:16:49 -0700

    added java class and begun tweaking xml

---
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/ProfileManagementFragment.java
@@ -0,0 +1,11 @@
+package edu.csuci.myci.cashflow;
+
+/**
+ * Created by keith.downer897 on 4/9/18.
+ */
+
+public class ProfileManagementFragment {
+    public ProfileManagementFragment() {
+
+    }
+}
