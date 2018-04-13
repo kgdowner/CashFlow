cecc3d0642c72c5ee02be56c9bf54ac58b679749
ViktoriyaR <satorit1@yahoo.com>
2018-04-12 23:16:01 -0700

    removed all category references, and added back the random UUID

--- CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Transaction.java
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Transaction.java
@@ -1,16 +1,23 @@
 package edu.csuci.myci.cashflow;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.Set;
+import java.util.UUID;
 
 
 // A transaction of some negative or positive amount.
 
 public class Transaction implements Serializable {
+    private SQLiteDatabase mDatabase;
+
     private BigDecimal amount;
-    private Set<Category> categories;
-    private TransactionID id;
+    //private Set<Category> categories;
+    private Date date;
+    private UUID id;
 
     private String name;
 
@@ -20,25 +27,28 @@ public class Transaction implements Serializable {
         setAmount(amount);
 
         // set the categories (if they were passed)
-        if(categories != null) {setCategories(categories);}
+        //if(categories != null) {setCategories(categories);}
 
         // create the transaction ID
-        this.id = new TransactionID();
+        this.id = (UUID.randomUUID());
 
         //set name
         setName(name);
+
+        this.date = new Date();
     }
+
 
     // accessors
     public BigDecimal getAmount() {
         return this.amount;
     }
 
-    public Set<Category> getCategories() {
-        return this.categories;
-    }
+//    public Set<Category> getCategories() {
+//        return this.categories;
+//    }
 
-    public TransactionID getID() {
+    public UUID getID() {
         return this.id;
     }
 
@@ -55,20 +65,28 @@ public class Transaction implements Serializable {
         this.amount = amount;
     }
 
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
+//    public void setCategories(Set<Category> categories) {
+//        this.categories = categories;
+//    }
 
-    public void setID(TransactionID id) {
+    public void setID(UUID id) {
         this.id = id;
     }
 
     // additional methods
-    public void addCategory(Category c) {
-        this.categories.add(c);
+//    public void addCategory(Category c) {
+//        this.categories.add(c);
+//    }
+//
+//    public void removeCateogry(Category c) {
+//        this.categories.remove(c);
+//    }
+
+    public Date getDate() {
+        return date;
     }
 
-    public void removeCateogry(Category c) {
-        this.categories.remove(c);
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
