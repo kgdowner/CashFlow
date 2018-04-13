057a2585b22acab7128fe4e2abccd1d053fa9d86
ViktoriyaR <satorit1@yahoo.com>
2018-04-12 23:22:40 -0700

    CategoryList is wrapper for retrieving category names from database

--- CashFlow/app/src/main/java/edu/csuci/myci/cashflow/CategoryList.java
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/CategoryList.java
@@ -1,9 +1,18 @@
 package edu.csuci.myci.cashflow;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu.csuci.myci.cashflow.database.TransactionBaseHelper;
+import edu.csuci.myci.cashflow.database.TransactionCursorWrapper;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema.CategoryTable;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema.CategoryTransactionTable;
 
 /**
  * Created by viktoriya on 4/9/18.
@@ -12,8 +21,8 @@ import java.util.List;
 public class CategoryList {
     private static CategoryList sCategoryList;
 
-
-    private List<Category> mCategories;
+    private SQLiteDatabase mDatabase;
+    private List<String> mCategories;
 
     public static CategoryList get(Context context) {
         if(sCategoryList == null) {
@@ -24,29 +33,84 @@ public class CategoryList {
     }
 
     private CategoryList(Context context){
-        this.mCategories = new ArrayList<>();
-        mCategories.add(new Category("groceries", 0));
-        mCategories.add(new Category("gas", 1));
-        mCategories.add(new Category("bullshit", 2));
-        mCategories.add(new Category("housing", 3));
-        mCategories.add(new Category("clothes", 4));
-        mCategories.add(new Category("drinking", 5));
-        mCategories.add(new Category("makeup", 6));
-        mCategories.add(new Category("computer", 7));
+        mDatabase =new TransactionBaseHelper(context).getWritableDatabase();
+
+
+//        this.mCategories = new ArrayList<>();
+        addCategory(new Category("groceries", 0));
+        addCategory(new Category("gas", 1));
+        addCategory(new Category("bullshit", 2));
+        addCategory(new Category("housing", 3));
+        addCategory(new Category("clothes", 4));
+        addCategory(new Category("drinking", 5));
+        addCategory(new Category("makeup", 6));
+        addCategory(new Category("computer", 7));
     }
 
-    public List<Category> getCategories() {
+    public List<String> getCategories() {
+        mCategories = new ArrayList<String>();
+        TransactionCursorWrapper cursor = querryCategories(null, null);
+
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()){
+                mCategories.add(cursor.getCategory().getCategoryName());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
         return mCategories;
+
+
+
     }
 
 
     public void addCategory(Category t){
-        mCategories.remove(t);
+
+        ContentValues values = getContentValues(t);
+        mDatabase.insert(CategoryTable.NAME, null, values);
     }
-    public void removeCategory(Category t){mCategories.add(t);}
+    private static ContentValues getContentValues(Category crime){
+        ContentValues values = new ContentValues();
+        values.put(CategoryTable.Cols.IDCATEGORY, crime.getCategoryId());
+        values.put(CategoryTable.Cols.CATEGORYNAME, crime.getCategoryName());
+
+
+        return values;
+
+    }
+
+    public void removeCategory(Category t){
+      //  mCategories.remove(t);
+    }
 
 
     //            Category tempCat = new Category("category"+i, i);
 //            Set<Category> tempCats = new HashSet();
 //            tempCats.add(tempCat);
+
+    private TransactionCursorWrapper querryCategories(String whereClause, String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                CategoryTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new TransactionCursorWrapper(cursor);
+    }
+    public void addCategoryTransaction(int categoryId, String transactionId){
+        ContentValues values = new ContentValues();
+        values.put(CategoryTransactionTable.Cols.IDCATEGORY, categoryId);
+        values.put(CategoryTransactionTable.Cols.IDTRANSACTION, transactionId);
+        mDatabase.insert(CategoryTransactionTable.NAME, null, values);
+
+    }
+
+    //add category /transaction combination
+    //remove category/transaction
 }
