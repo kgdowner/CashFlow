043c460d3a0c9a030c29309a7b4cc5a7b746daae
ViktoriyaR <satorit1@yahoo.com>
2018-04-12 23:18:20 -0700

    modified Profile.java to work with database

--- CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Profile.java
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Profile.java
@@ -1,6 +1,9 @@
 package edu.csuci.myci.cashflow;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.math.BigDecimal;
 import java.util.Set;
@@ -9,15 +12,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import edu.csuci.myci.cashflow.database.TransactionBaseHelper;
+import edu.csuci.myci.cashflow.database.TransactionCursorWrapper;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema.CategoryTable;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema.CategoryTransactionTable;
+import edu.csuci.myci.cashflow.database.TransactionDbSchema.TransactionTable;
+
 /**
  * Created by viktoriya on 3/3/18.
  */
 
 public class Profile {
     private static Profile sProfile;
-    //s prefix is for Static Variables...
-
+    private Context mContext;
     private List<Transaction> mTransactions;
+    private SQLiteDatabase mDatabase;
+
 
     public static Profile get(Context context) {
         if(sProfile == null) {
@@ -27,7 +38,9 @@ public class Profile {
 
     }
     private Profile(Context context) {
-        this.mTransactions = new ArrayList<>();
+//        this.mTransactions = new ArrayList<>();
+        mContext = context.getApplicationContext();
+        mDatabase = new TransactionBaseHelper(mContext).getWritableDatabase();
 //        for(int i = 0; i<100; i++){
 //            // TODO: remove this & implement non-hardcoded list
 //            Category tempCat = new Category("category"+i, i);
@@ -41,20 +54,107 @@ public class Profile {
 //        }
     }
 
-    public List<Transaction> getTransactions() {
-        return mTransactions;
-    }
+   public List<Transaction> getTransactions() {
+//
+       List<Transaction> transactions = new ArrayList<>();
+
+       TransactionCursorWrapper cursor = querryCrimes(null, null);
+
+       try {
+           cursor.moveToFirst();
+           while (!cursor.isAfterLast()){
+               transactions.add(cursor.getTransaction());
+               cursor.moveToNext();
+           }
+       } finally {
+           cursor.close();
+       }
+       return transactions;
+   }
 
     public Transaction getTransactions(UUID id) {
-        for(Transaction transaction : mTransactions){
-            if(transaction.getID().equals(id)) {
-                return transaction;
+        TransactionCursorWrapper cursor = querryCrimes(
+                TransactionTable.Cols.IDTRANSACTION + " = ?",
+                new String[]{id.toString()}
+        );
+
+        try {
+            if (cursor.getCount() == 0){
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getTransaction();
+        } finally {
+            cursor.close();
         }
-        return null;
     }
+
+    public ArrayList<String> getAllCategoriesForTransaction(String transactionId){
+        //find in my Transaction/Category Table  all Category ids that belong to this transaction
+        //search category table for all id / catName combos,
+        //concatenate catName combos to string....
+        ArrayList<String> catNames = new ArrayList<>();
+
+        String query = "SELECT * FROM Categories, Cat_Transaction " +
+                "WHERE Cat_Transaction.idTransaction =? " +
+                "AND Categories.idCategory =  Cat_Transaction.idCategory";
+        Cursor c = mDatabase.rawQuery(query, new String[]{transactionId});
+        if (c.moveToFirst()) {
+            do {
+
+                catNames.add(c.getString(c.getColumnIndex(CategoryTable.Cols.CATEGORYNAME)));
+
+            } while (c.moveToNext());
+        }
+        return  catNames;
+
+
+    }
+
+    public void updateTransaction(Transaction crime){
+        String uuidString = crime.getID().toString();
+        ContentValues values = getContentValues(crime);
+
+        mDatabase.update(TransactionTable.NAME, values,
+                TransactionTable.Cols.IDTRANSACTION + " = ?",
+                new String[] {uuidString});
+
+    }
+
+
     public void removeTransaction(Transaction t){
-        mTransactions.remove(t);
+        //mTransactions.remove(t);
+        String uuidString = t.getID().toString();
+        mDatabase.delete(TransactionTable.NAME, TransactionTable.Cols.IDTRANSACTION + " = ?", new String[] {uuidString});
+
     }
-    public void addTransaction(Transaction t){mTransactions.add(t);}
+    public void addTransaction(Transaction t){
+        //mTransactions.add(t);
+        ContentValues values = getContentValues(t);
+        mDatabase.insert(TransactionTable.NAME, null, values);
+    }
+    private static ContentValues getContentValues(Transaction crime){
+        ContentValues values = new ContentValues();
+        values.put(TransactionTable.Cols.IDTRANSACTION, crime.getID().toString());
+        values.put(TransactionTable.Cols.TITLE, crime.getName());
+        values.put(TransactionTable.Cols.DATE, crime.getDate().toString());
+        values.put(TransactionTable.Cols.AMOUNT, crime.getAmount().toString());
+
+        return values;
+
+    }
+    private TransactionCursorWrapper querryCrimes(String whereClause, String[] whereArgs){
+        Cursor cursor = mDatabase.query(
+                TransactionTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+        return new TransactionCursorWrapper(cursor);
+    }
+
+
 }
