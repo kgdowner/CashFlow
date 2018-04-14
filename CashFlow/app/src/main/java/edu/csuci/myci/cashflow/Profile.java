84e464cfb9508bb86a3c44aea04361ed745fb773
kgdowner <keith.downer897@myci.csuci.edu>
2018-04-14 03:43:32 -0700

    rewrote Profile class

--- CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Profile.java
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/Profile.java
@@ -1,60 +1,18 @@
 package edu.csuci.myci.cashflow;
 
-import android.content.Context;
-
-import java.math.BigDecimal;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-/**
- * Created by viktoriya on 3/3/18.
- */
-
 public class Profile {
-    private static Profile sProfile;
-    //s prefix is for Static Variables...
+    private String name;
+    //private <????> database;  // TODO: this is where the database class is referenced
 
-    private List<Transaction> mTransactions;
+    public Profile(String name /*, <???> database*/) {
+        this.name = name;
 
-    public static Profile get(Context context) {
-        if(sProfile == null) {
-            sProfile = new Profile(context);
-        }
-        return  sProfile;
-
-    }
-    private Profile(Context context) {
-        this.mTransactions = new ArrayList<>();
-//        for(int i = 0; i<100; i++){
-//            // TODO: remove this & implement non-hardcoded list
-//            Category tempCat = new Category("category"+i, i);
-//            Set<Category> tempCats = new HashSet();
-//            tempCats.add(tempCat);
-//
-//            Transaction transaction = new Transaction(new BigDecimal(0.00), tempCats);
-//            transaction.setAmount(new BigDecimal(0.25 * i));
-//
-//            this.mTransactions.add(transaction);
-//        }
+        // TODO: set this.database reference & any final setup for the database
     }
 
-    public List<Transaction> getTransactions() {
-        return mTransactions;
+    public String getName() {
+        return this.name;
     }
 
-    public Transaction getTransactions(UUID id) {
-        for(Transaction transaction : mTransactions){
-            if(transaction.getID().equals(id)) {
-                return transaction;
-            }
-        }
-        return null;
-    }
-    public void removeTransaction(Transaction t){
-        mTransactions.remove(t);
-    }
-    public void addTransaction(Transaction t){mTransactions.add(t);}
+    // TODO: any methods the database might require for setup \ utilization
 }
