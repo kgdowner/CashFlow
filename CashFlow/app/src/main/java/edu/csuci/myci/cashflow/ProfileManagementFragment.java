c1c9b1e481a2ab1c8451d66dbf9f65211c03fb8d
kgdowner <keith.downer897@myci.csuci.edu>
2018-04-18 13:19:11 -0700

    profile management fragment now updates active list

--- CashFlow/app/src/main/java/edu/csuci/myci/cashflow/ProfileManagementFragment.java
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/ProfileManagementFragment.java
@@ -4,14 +4,15 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
 
-/**
- * Created by keith.downer897 on 4/9/18.
- */
+
 
 public class ProfileManagementFragment {
     private Dialog active_dialog;
 
+    private RadioGroup profiles;
     private Button button_add_profile;
     private Button button_remove_profile;
     private Button button_cancel;
@@ -22,32 +23,45 @@ public class ProfileManagementFragment {
         this.active_dialog = new Dialog(context);
         this.active_dialog.setContentView(R.layout.dialog_profile_management);
 
-        // acquire the button references
+        // acquire layout object references
+        this.profiles               = (RadioGroup) this.active_dialog.findViewById(R.id.radio_group);
         this.button_add_profile     = (Button) this.active_dialog.findViewById(R.id.button_add_profile);
         this.button_remove_profile  = (Button) this.active_dialog.findViewById(R.id.button_remove_profile);
         this.button_cancel          = (Button) this.active_dialog.findViewById(R.id.button_cancel);
 
-        // set button listeners  -  FIXME: move to private methods and register those here
-        this.button_add_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        // register button listener functions
+        this.button_add_profile.setOnClickListener(     new View.OnClickListener() {@Override public void onClick(View v) {onButtonAddProfile();    }});
+        this.button_remove_profile.setOnClickListener(  new View.OnClickListener() {@Override public void onClick(View v) {onButtonRemoveProfile(); }});
+        this.button_cancel.setOnClickListener(          new View.OnClickListener() {@Override public void onClick(View v) {onButtonCancel();        }});
 
-        this.button_remove_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // update the profile list
+        RadioButton defaultProfile = (RadioButton) this.active_dialog.findViewById(R.id.default_radio_button);
+        boolean first = true;
+        for(String profileName : GlobalScopeContainer.profileList) {
+            if(first) {
+                defaultProfile.setText(profileName);
+                first = false;
+            } else {
+                RadioButton newButton = new RadioButton(context);
+                newButton.setText(profileName);
+                newButton.setLayoutParams(defaultProfile.getLayoutParams());
+                this.profiles.addView(newButton);
             }
-        });
-
-        this.button_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                active_dialog.dismiss();  // FIXME: referencing with 'this' keyword is out of scope
-            }
-        });
+        }
 
         // show the dialog
         this.active_dialog.show();
+    }
+
+    private void onButtonAddProfile() {
+        // TODO: add database here
+    }
+
+    private void onButtonRemoveProfile() {
+        // TODO: remove database here + confirm dialog
+    }
+
+    private void onButtonCancel() {
+        this.active_dialog.dismiss();
     }
 }
