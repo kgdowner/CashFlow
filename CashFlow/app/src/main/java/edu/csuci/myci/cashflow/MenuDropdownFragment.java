a1309e3b9f8f44cf3b1aadb5d08c1092b1637399
kgdowner <keith.downer897@myci.csuci.edu>
2018-05-02 02:02:14 -0700

    basic shell for menu dropdown fragment code

---
+++ CashFlow/app/src/main/java/edu/csuci/myci/cashflow/MenuDropdownFragment.java
@@ -0,0 +1,46 @@
+package edu.csuci.myci.cashflow;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MenuDropdownFragment extends DialogFragment {
+    public static void display(Context context) {
+        DialogFragment df = new MenuDropdownFragment();
+        FragmentTransaction ft = ((FragmentActivity) context).getSupportFragmentManager().beginTransaction();
+        df.show(ft, "MenuDropdownFragment");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setStyle(DialogFragment.STYLE_NORMAL, R.style.CustomDialog);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_profile_management, container, true);
+        // create the dialog and assign it the proper layout
+        // ... ?  (TODO?)
+
+        // get button handles
+        Button menuTitle = (Button) view.findViewById(R.id.dropdown_menu_title);
+
+        // register button listener functions
+        menuTitle.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {onMenuTitle();}});
+
+        // show the dialog
+        return view;
+    }
+
+    // button functions
+    private void onMenuTitle() {
+
+    }
+}
